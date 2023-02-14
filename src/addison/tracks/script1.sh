f1268b70c482a1807c262c610bacec0c6fe3180f
Isaac Cheng <47993930+IsaacCheng9@users.noreply.github.com>
2023-02-14 03:41:35 +0000

    Move scripts to tracks folder

--- src/addison/tracks/script1.sh
+++ src/addison/tracks/script1.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 ID="Everybody+(Backstreet's+Back)+(Radio+Edit)"
 ESCAPED=`perl -e "use URI::Escape; print uri_escape(\"$ID\")"`
-AUDIO=`base64 -i "$ID".wav`
+# AUDIO=`base64 -i "$ID".wav`
+# TODO: Change this back to the previous command when testing on SSH.
+AUDIO=`base64 -e "$ID".wav`
 RESOURCE=localhost:3000/tracks/$ESCAPED
 echo "{ \"Id\":\"$ID\", \"Audio\":\"$AUDIO\" }" > input
 curl -v -X PUT -d @input $RESOURCE 
