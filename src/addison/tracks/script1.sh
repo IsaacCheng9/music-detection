76660a06b5ac7d30241f72e4d53df2a10a0d42e5
Isaac Cheng <47993930+IsaacCheng9@users.noreply.github.com>
2023-02-16 15:08:26 +0000

    Fix RC 500 by using GNU base64

--- src/addison/tracks/script1.sh
+++ src/addison/tracks/script1.sh
@@ -3,7 +3,7 @@ ID="Everybody+(Backstreet's+Back)+(Radio+Edit)"
 ESCAPED=`perl -e "use URI::Escape; print uri_escape(\"$ID\")"`
 # AUDIO=`base64 -i "$ID".wav`
 # TODO: Change this back to the previous command when testing on SSH.
-AUDIO=`base64 -e "$ID".wav`
+AUDIO=`gbase64 -i "$ID".wav`
 RESOURCE=localhost:3000/tracks/$ESCAPED
 echo "{ \"Id\":\"$ID\", \"Audio\":\"$AUDIO\" }" > input
 curl -v -X PUT -d @input $RESOURCE 
