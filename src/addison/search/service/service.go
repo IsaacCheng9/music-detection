58028fcaa244c0b95c9571ea728b7d0fef9803cf
Isaac Cheng <47993930+IsaacCheng9@users.noreply.github.com>
2023-02-16 15:32:03 +0000

    Get AudD API token from untracked .txt file

--- src/addison/search/service/service.go
+++ src/addison/search/service/service.go
@@ -1,17 +1,32 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"io"
+	"log"
 	"net/http"
 	"net/url"
+	"os"
 )
+
+// Get the API token from token.txt.
+func getAPIToken() string {
+	file, err := os.Open("../token.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	scanner.Scan()
+	return scanner.Text()
+}
 
 func main() {
 	data := url.Values{
-		"url":       {"https://audd.tech/example.mp3"},
-		"return":    {"apple_music,spotify"},
-		"api_token": {"test"},
+		"url":    {"https://audd.tech/example.mp3"},
+		"return": {"apple_music,spotify"},
+		"api_token": {getAPIToken()},
 	}
 	response, _ := http.PostForm("https://api.audd.io/", data)
 	defer response.Body.Close()
