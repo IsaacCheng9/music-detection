b31c29b11e483ceb42f642de43f0c634da36dbad
Isaac Cheng <47993930+IsaacCheng9@users.noreply.github.com>
2023-02-17 17:54:42 +0000

    Fix script for MacOS environemnt

--- src/addison/tests/script5.sh
+++ src/addison/tests/script5.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 ID="~Everybody+(Backstreet's+Back)+(Radio+Edit)"
-AUDIO=`base64 -i "$ID".wav`
+# AUDIO=`base64 -i "$ID".wav`
+# TODO: Change this back to test in Linux environment.
+AUDIO=`gbase64 -i "$ID".wav`
 RESOURCE=localhost:3001/search
 echo "{ \"Audio\":\"$AUDIO\" }" > input
 curl -v -X POST -d @input $RESOURCE
