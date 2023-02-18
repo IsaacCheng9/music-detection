5b0210988e41db845e0a4b460540a8cc267ba0ae
Isaac Cheng <47993930+IsaacCheng9@users.noreply.github.com>
2023-02-18 00:38:56 +0000

    Get body from POST request and recognise track

--- src/addison/search/resources/resources.go
+++ src/addison/search/resources/resources.go
@@ -9,15 +9,37 @@ import (
 )
 
 func searchTrack(w http.ResponseWriter, r *http.Request) {
-	base64Audio := r.FormValue("Audio")
-	result := service.SearchAudDTracksAPI(base64Audio)
-	w.WriteHeader(200) /* OK */
-	json.NewEncoder(w).Encode(result)
+	//base64Audio := r.FormValue("Audio")
+	// Should work with the script:
+	// ID="~Everybody+(Backstreet ’s+Back)+(Radio+Edit)" AUDIO=‘base64 -i "$ID".wav‘
+	// RESOURCE=localhost:3001/search
+	// echo "{ \"Audio\":\"$AUDIO\" }" > input curl -v -X POST -d @input $RESOURCE
+	t := map[string]interface{}{}
+	if err := json.NewDecoder(r.Body).Decode(&t); err == nil {
+		if base64audio, ok := t["Audio"].(string); ok {
+			if title, err := service.SearchAudDTracksAPI(base64audio); err == nil {
+				u := map[string]interface{}{"Id": title}
+				w.WriteHeader(http.StatusOK)
+				json.NewEncoder(w).Encode(u)
+				return
+			} else {
+				w.WriteHeader(http.StatusInternalServerError)
+				return
+			}
+		}
+	}
+	w.WriteHeader(http.StatusBadRequest)
+
+	//vars := mux.Vars(r)
+	//base64Audio := vars["Audio"]
+	//fmt.Println(base64Audio)
+	//result := service.SearchAudDTracksAPI(base64Audio)
+	//w.WriteHeader(200) /* OK */
+	//json.NewEncoder(w).Encode(result)
 }
 
 func Router() http.Handler {
 	r := mux.NewRouter()
-	/* controller */
 	r.HandleFunc("/search", searchTrack).Methods("POST")
 	return r
 }
--- src/addison/search/service/service.go
+++ src/addison/search/service/service.go
@@ -2,11 +2,12 @@ package service
 
 import (
 	"bufio"
-	"fmt"
+	"encoding/json"
+	"errors"
 	"log"
+	"net/http"
+	"net/url"
 	"os"
-
-	"github.com/AudDMusic/audd-go"
 )
 
 // Get the API token from token.txt.
@@ -26,18 +27,46 @@ func getAPIToken() string {
 	return scanner.Text()
 }
 
-func SearchAudDTracksAPI(base64Audio string) string {
-	client := audd.NewClient(getAPIToken())
-	url := "https://audd.tech/example.mp3"
-	additionalParams := map[string]string{"audio": base64Audio}
-	result, err := client.Recognize(url, "apple_music,spotify", additionalParams)
-	if err != nil {
-		panic(err)
+func titleOf(t map[string]interface{}) (string, error) {
+	if response, ok := t["result"].(map[string]interface{}); ok {
+		if title, ok := response["title"].(string); ok {
+			return title, nil
+		}
 	}
-	fmt.Printf("%s - %s.\nTimecode: %s, album: %s. ℗ %s, %s\n\n"+
-		"Listen: %s\nOr directly on:\n- Apple Music: %s, \n- Spotify: %s",
-		result.Artist, result.Title, result.Timecode, result.Album,
-		result.Label, result.ReleaseDate, result.SongLink,
-		result.AppleMusic.URL, result.Spotify.ExternalUrls.Spotify)
-	return result.Title
+	return "", errors.New("titleOf")
+}
+
+func SearchAudDTracksAPI(base64Audio string) (string, error) {
+	//client := audd.NewClient(getAPIToken())
+	//client := audd.NewClient("b30fbcc812b45d5379114bd1f430a32c")
+	//url := "https://audd.tech/example.mp3"
+	//additionalParams := map[string]string{"audio": base64Audio}
+	//result, err := client.Recognize(url, "apple_music,spotify", additionalParams)
+	//if err != nil {
+	//	panic(err)
+	//}
+	//fmt.Printf("%s - %s.\nTimecode: %s, album: %s. ℗ %s, %s\n\n"+
+	//	"Listen: %s\nOr directly on:\n- Apple Music: %s, \n- Spotify: %s",
+	//	result.Artist, result.Title, result.Timecode, result.Album,
+	//	result.Label, result.ReleaseDate, result.SongLink,
+	//	result.AppleMusic.URL, result.Spotify.ExternalUrls.Spotify)
+	//return result.Title
+
+	data := url.Values{
+		"api_token": {"b30fbcc812b45d5379114bd1f430a32c"},
+		"audio":     {base64Audio},
+		"return":    {"apple_music,spotify"},
+	}
+	response, _ := http.PostForm("https://api.audd.io/", data)
+	defer response.Body.Close()
+	//body, _ := io.ReadAll(response.Body)
+	//fmt.Println(string(body))
+	t := map[string]interface{}{}
+	if err := json.NewDecoder(response.Body).Decode(&t); err == nil {
+		if title, err := titleOf(t); err == nil {
+			return title, nil
+		}
+	}
+
+	return "", errors.New("SearchAudDTracksAPI")
 }
