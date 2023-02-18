301e8fc174fd591fec9093e00d3cdfd6a106e574
Isaac Cheng <47993930+IsaacCheng9@users.noreply.github.com>
2023-02-18 01:49:52 +0000

    Fix script for MacOS environment

--- src/addison/tests/script6.sh
+++ src/addison/tests/script6.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 ID="~Everybody+(Backstreet's+Back)+(Radio+Edit)"
-AUDIO=`base64 -i "$ID".wav`
+# AUDIO=`base64 -i "$ID".wav`
+# TODO: Change this back to work for Linux environment.
+AUDIO=`gbase64 -i "$ID".wav`
 URL=localhost:3002/cooltown
 echo "{ \"Audio\":\"$AUDIO\" }" > input
 curl -v -X POST -d @input $URL
