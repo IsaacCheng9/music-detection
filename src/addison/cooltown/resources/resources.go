e7d5b501db8b4d03cd0129a4f3228a95be7a6182
Isaac Cheng <47993930+IsaacCheng9@users.noreply.github.com>
2023-02-20 14:22:58 +0000

    Fix RCs for getting track from fragment

--- src/addison/cooltown/resources/resources.go
+++ src/addison/cooltown/resources/resources.go
@@ -12,25 +12,38 @@ func getTrackFromFragment(w http.ResponseWriter, r *http.Request) {
 	var id string
 	if err := json.NewDecoder(r.Body).Decode(&t); err == nil {
 		if base64Audio, ok := t["Audio"].(string); ok {
-			if title, err := service.GetIdFromAudioFragment(base64Audio); err == nil {
+			if title, err := service.GetIdFromAudioFragment(base64Audio); err == nil && title != "" {
 				id = title
+			} else if err != nil {
+				// 500 Internal Server Error - the server was unable to process
+				// the request.
+				w.WriteHeader(500)
+				return
 			} else {
-				w.WriteHeader(http.StatusNotFound)
+				// 404 Not Found - no matching track and ID for the audio
+				// fragment was found.
+				w.WriteHeader(404)
 				return
 			}
 		}
 	} else {
-		w.WriteHeader(http.StatusBadRequest)
+		// 400 Bad Request - the request could not be decoded by the server due
+		// to malformed syntax.
+		w.WriteHeader(400)
 	}
 
-	if audio, err := service.GetAudioFromId(id); err == nil {
+	if audio, err := service.GetAudioFromId(id); err == nil && audio != "" {
 		u := map[string]interface{}{"Audio": audio}
-		w.WriteHeader(http.StatusOK)
 		json.NewEncoder(w).Encode(u)
-		return
+		// 200 OK - the matching track for the audio fragment has been found.
+		w.WriteHeader(200)
+	} else if err != nil {
+		// 500 Internal Server Error - the server was unable to process the
+		// request.
+		w.WriteHeader(500)
 	} else {
-		w.WriteHeader(http.StatusNotFound)
-		return
+		// 404 Not Found - no matching track for the audio fragment was found.
+		w.WriteHeader(404)
 	}
 }
 
